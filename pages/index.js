f83752860e34c4a9be3d52e19c4228f90fca0a86
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-10-11 01:32:58 +0800

    test3

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ const Home = () => {
         {/* subtitle */}
         <motion.p variants={fadeIn('down', 0.3)} initial="hidden" animate="show"
         exit="hidden" className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 text-2xl xl:mb-16 z-10'>
-          Hi, I am
+          Hi!, I am
           <TypeAnimation
             preRenderFirstString={true}
             sequence={[
