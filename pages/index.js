49b8bc180eec95ead5a32a5535ca0203105a99ff
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-10-27 10:52:43 +0800

    Btn.Motion.Test

--- pages/index.js
+++ pages/index.js
@@ -60,12 +60,22 @@ const Home = () => {
         {/* btn */}
         {/* <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show"
         exit="hidden" className='flex justify-center xl:hidden relative'> */}
-        <button
+        <motion.button 
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg"
+            id="btn"
+        >
+            Button
+        </motion.button>
+        {/* <button
           className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
           id="btn"
         >
           Button
-        </button>
+        </button> */}
         {/* </motion.div> */}
         {/* <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show"
         exit="hidden" className='flex justify-center xl:hidden relative'>
