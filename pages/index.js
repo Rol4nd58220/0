5c1de864a7bc9522118a0e7d0cdf589d1afd8101
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-10-27 11:18:38 +0800

    Btn.Test1

--- pages/index.js
+++ pages/index.js
@@ -61,10 +61,10 @@ const Home = () => {
         {/* btn */}
         {/* <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show"
         exit="hidden" className='flex justify-center xl:hidden relative'> */}
-        <Link href="./about">
+        <Link href={'./about'}>
           <motion.button 
-              variants={fadeIn('down', 0.4)}
-              initial="hidden"
+              variants={fadeIn('down', 0.3)} 
+              initial="hidden" 
               animate="show"
               exit="hidden"
               className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
