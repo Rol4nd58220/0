a1445ba56e3074454bcd591169333ed7be3c1b79
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-10-27 11:14:15 +0800

    Btn.NewLink.Test1

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { TypeAnimation } from 'react-type-animation';
+import Link from 'next/link';
 
 
 // next iamge
@@ -60,16 +61,18 @@ const Home = () => {
         {/* btn */}
         {/* <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show"
         exit="hidden" className='flex justify-center xl:hidden relative'> */}
-        <motion.button 
-            variants={fadeIn('down', 0.4)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
-            id="btn"
-        >
-            Button
-        </motion.button>
+        <Link href="./about">
+          <motion.button 
+              variants={fadeIn('down', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
+              id="btn"
+          >
+              Button
+          </motion.button>
+        </Link>
         {/* <button
           className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
           id="btn"
