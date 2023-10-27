3e532137289f0a8bdca77161d536bd740806eee6
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-10-27 20:23:30 +0800

    Fuck

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -12,7 +12,7 @@ const ProjectsBtn = () => {
   return (
     <div className='mx-auto xl:mx-0 z-10'>
       <Link href={'./about'} passHref>
-        <a className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-red-500 hover:border-red-500 hover:text-shadow-lg z-10 animate-chitchat"
+        <a className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-red-500 hover:border-red-500 hover:text-shadow-lg z-10"
           id="btn"
         >
           About Me
