df2ce996d9289a04fdbf9879527da91f97ee7341
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-10-27 13:08:36 +0800

    Btn.Final

--- pages/index.js
+++ pages/index.js
@@ -59,26 +59,7 @@ const Home = () => {
           />
         </motion.p>
         {/* btn */}
-        {/* <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show"
-        exit="hidden" className='flex justify-center xl:hidden relative'> */}
-        {/* <Link href={'./about'} className='mx-auto xl:mx-0 z-10'>
-          <motion.button 
-              variants={fadeIn('down', 0.3)} 
-              initial="hidden" 
-              animate="show"
-              exit="hidden"
-              className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
-              id="btn"
-          >
-              Button
-          </motion.button>
-        </Link> */}
-        {/* <button
-          className="w-40 px-5 py-2 uppercase rounded-md text-sm font-semibold text-white text-opacity-80 bg-transparent border border-white border-opacity-80 transition-transform duration-500 ease-in-out select-none hover:text-opacity-100 hover:bg-blue-500 hover:border-blue-500 hover:text-shadow-lg z-10"
-          id="btn"
-        >
-          Button
-        </button> */}
+        
         {/* </motion.div> */}
         <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show"
         exit="hidden" className='flex justify-center xl:hidden relative'>
