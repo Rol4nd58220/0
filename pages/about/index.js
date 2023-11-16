96f9561380db21863906ba10f3ebb87c1120fc31
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 07:21:16 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -12,10 +12,12 @@ import {
 import Circles from '../../components/Circles';
 
 const SkillBar = ({ skill, level }) => (
-  <div className="flex items-center justify-center mb-4">
-    <span className="mr-2 text-lg font-medium">{skill}</span>
-    <div className="w-full bg-gray-200 rounded-full h-4">
-      <div className="bg-blue-600 h-4 rounded-full" style={{ width: `${level}%` }}></div>
+  <div className="mb-4">
+    <div className="flex items-center justify-center mb-4">
+      <span className="mr-2 text-lg font-medium">{skill}</span>
+      <div className="w-full bg-gray-200 rounded-full h-4">
+        <div className="bg-blue-600 h-4 rounded-full" style={{ width: `${level}%` }}></div>
+      </div>
     </div>
   </div>
 );
@@ -32,7 +34,7 @@ const About = () => {
   return (
     <div className="container mx-auto flex flex-col lg:flex-row items-center justify-around py-6">
       {/* Bio Section */}
-      <div className="lg:w-1/4 px-4 mt-4 lg:mt-20 text-center">
+      <div className="lg:w-1/4 px-4 mt-4 lg:mt-50 text-center">
         <h2 className="text-2xl font-bold">About Me</h2>
         <p className="mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
         <h3 className="text-xl font-semibold mt-6">Contact Details</h3>
@@ -45,7 +47,7 @@ const About = () => {
       </div>
 
       {/* Image Section */}
-      <div className="lg:w-1/4 px-4 mt-4 lg:mt-20 flex justify-center">
+      <div className="lg:w-1/4 px-4 mt-4 lg:mt-50 flex justify-center">
         <Image 
           src="/avatar2.png" 
           alt="Profile Picture" 
@@ -56,7 +58,7 @@ const About = () => {
       </div>
 
       {/* Skills Section */}
-      <div className="skills-section lg:w-1/4 px-4 mt-4 lg:mt-20 text-center">
+      <div className="skills-section lg:w-1/4 px-4 mt-4 lg:mt-50 text-center">
         <h3 className="text-2xl font-bold">My Skills</h3>
         {skills.map((skill, index) => (
           <SkillBar key={index} skill={skill.name} level={skill.level} />
