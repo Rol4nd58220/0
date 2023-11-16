f5017702903b971e3a250d3b14a0d2bf036e49c2
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 06:16:38 +0800

    avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import React, { useState } from "react";
+import Image from 'next/image';
 
 // icons
 import {
@@ -114,13 +114,15 @@ const About = () => {
         <div>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
             {aboutData.map((item, itemIndex) => (
+              return (
               <div 
                 key={itemIndex} 
-                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300' : ''} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`} 
+                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300':} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`} 
                 onClick={() => setIndex(itemIndex)}
               > 
-                {item.title}
+                  {item.title}
               </div>
+            );
             ))}
           </div>
         </div>
