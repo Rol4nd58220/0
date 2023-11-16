89570ba2266205cf76705b53aba0e73c7f3befb2
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 06:18:21 +0800

    avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -114,7 +114,6 @@ const About = () => {
         <div>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
             {aboutData.map((item, itemIndex) => (
-              return (
               <div 
                 key={itemIndex} 
                 className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300':} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`} 
@@ -122,7 +121,6 @@ const About = () => {
               > 
                   {item.title}
               </div>
-            );
             ))}
           </div>
         </div>
