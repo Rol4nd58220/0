fcca247c91a56125219bcb5b517991780a5b4e62
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 07:47:06 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -14,7 +14,7 @@ import Circles from '../../components/Circles';
 // SkillBar component for displaying skills
 const SkillBar = ({ skill, level }) => (
   <div className="mb-6">
-    <div className="text-lg font-medium text-right">{skill}</div>
+    <div className="text-lg font-medium text-left">{skill}</div>
     <div className="mt-2 w-full bg-gray-200 rounded-full h-4">
       <div className="bg-blue-600 h-4 rounded-full" style={{ width: `${level}%` }}></div>
     </div>
@@ -34,15 +34,15 @@ const About = () => {
     <div className="container mx-auto py-6 h-full">
       <div className="flex flex-col lg:flex-row justify-around items-center h-full">
         {/* Bio Section */}
-        <div className="lg:w-1/4 px-4 flex flex-col justify-center text-right mx-auto">
+        <div className="lg:w-1/4 px-4 mx-auto">
           <h2 className="text-2xl font-bold">About Me</h2>
           <p className="mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
           <h3 className="text-xl font-semibold mt-6">Contact Details</h3>
           <ul className="list-none mt-4">
-            <li className="mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
-            <li className="mb-2"><FaPhone className="mr-2" /> +123456789</li>
-            <li className="mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
-            <li className="mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
+            <li className="flex justify-center mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
+            <li className="flex justify-center mb-2"><FaPhone className="mr-2" /> +123456789</li>
+            <li className="flex justify-center mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
+            <li className="flex justify-center mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
           </ul>
         </div>
 
@@ -58,7 +58,7 @@ const About = () => {
         </div>
 
         {/* Skills Section */}
-        <div className="skills-section lg:w-1/4 px-4 flex flex-col justify-center text-right mx-auto">
+        <div className="skills-section lg:w-1/4 px-4 text-center mx-auto">
           <h3 className="text-2xl font-bold">My Skills</h3>
           {skills.map((skill, index) => (
             <SkillBar key={index} skill={skill.name} level={skill.level} />
