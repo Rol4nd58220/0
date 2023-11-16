a34d08a6ff718f20b42916bc17d261fbfcc343d4
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 07:10:46 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -14,7 +14,7 @@ import Circles from '../../components/Circles';
 
 // SkillBar component for displaying skills
 const SkillBar = ({ skill, level }) => (
-  <div className="flex items-center mb-4">
+  <div className="flex items-center justify-center mb-4">
     <span className="mr-2 text-lg font-medium">{skill}</span>
     <div className="w-full bg-gray-200 rounded-full h-4">
       <div className="bg-blue-600 h-4 rounded-full" style={{ width: `${level}%` }}></div>
@@ -35,15 +35,15 @@ const About = () => {
   return (
     <div className="container mx-auto flex flex-col lg:flex-row items-center justify-around py-6">
       {/* Bio Section */}
-      <div className="lg:w-1/4 px-4 mb-6 lg:mb-0">
-        <h2 className="text-2xl font-bold text-left">About Me</h2>
-        <p className="text-left mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
-        <h3 className="text-xl font-semibold text-left mt-6">Contact Details</h3>
+      <div className="lg:w-1/4 px-4 mb-6 lg:mb-0 text-center">
+        <h2 className="text-2xl font-bold">About Me</h2>
+        <p className="mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
+        <h3 className="text-xl font-semibold mt-6">Contact Details</h3>
         <ul className="list-none mt-4">
-          <li className="flex items-center mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
-          <li className="flex items-center mb-2"><FaPhone className="mr-2" /> +123456789</li>
-          <li className="flex items-center mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
-          <li className="flex items-center mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
+          <li className="flex items-center justify-center mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
+          <li className="flex items-center justify-center mb-2"><FaPhone className="mr-2" /> +123456789</li>
+          <li className="flex items-center justify-center mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
+          <li className="flex items-center justify-center mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
         </ul>
       </div>
 
@@ -59,8 +59,8 @@ const About = () => {
       </div>
 
       {/* Skills Section */}
-      <div className="skills-section lg:w-1/4 px-4">
-        <h3 className="text-2xl font-bold text-left">My Skills</h3>
+      <div className="skills-section lg:w-1/4 px-4 text-center">
+        <h3 className="text-2xl font-bold">My Skills</h3>
         {skills.map((skill, index) => (
           <SkillBar key={index} skill={skill.name} level={skill.level} />
         ))}
