f3188d8990cb380808fc234180bb298510a5a5b2
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 05:59:54 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -96,6 +96,7 @@ import { fadeIn } from '../../variants'
 
 
 const About = () => {
+  const [index, setIndex] = useState(0);
   return (
     <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
         <motion.div variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden">
