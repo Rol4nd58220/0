f18a730856ef295ad7f5fd0cb839f264045df15c
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 07:06:50 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,32 +3,19 @@ import Image from 'next/image';
 
 // Icons
 import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
   FaEnvelope,
   FaPhone,
   FaLinkedin,
   FaGithub,
 } from "react-icons/fa";
 
-import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from "react-icons/si";
-
-// Circles component (assuming it's in your project)
+// Assuming Circles is a component in your project
 import Circles from '../../components/Circles';
 
-// SkillBar component
+// SkillBar component for displaying skills
 const SkillBar = ({ skill, level }) => (
   <div className="flex items-center mb-4">
-    <span className="mr-2">{skill}</span>
+    <span className="mr-2 text-lg font-medium">{skill}</span>
     <div className="w-full bg-gray-200 rounded-full h-4">
       <div className="bg-blue-600 h-4 rounded-full" style={{ width: `${level}%` }}></div>
     </div>
@@ -38,17 +25,17 @@ const SkillBar = ({ skill, level }) => (
 const About = () => {
   // Sample skills data
   const skills = [
-    { name: <FaHtml5 />, level: 90 },
-    { name: <FaCss3 />, level: 85 },
-    { name: <FaJs />, level: 80 },
-    { name: <FaReact />, level: 75 },
-    { name: <SiNextdotjs />, level: 70 },
+    { name: 'Programmer', level: 90 },
+    { name: '3D Artist', level: 85 },
+    { name: 'Web Developer', level: 80 },
+    { name: 'Cook', level: 75 },
+    { name: 'Artist', level: 70 },
   ];
 
   return (
-    <div className="bg-primary/60 h-full container mx-auto flex flex-col lg:flex-row items-center justify-between py-6">
+    <div className="container mx-auto flex flex-col lg:flex-row items-center justify-around py-6">
       {/* Bio Section */}
-      <div className="lg:w-1/3 px-4 mb-6 lg:mb-0">
+      <div className="lg:w-1/4 px-4 mb-6 lg:mb-0">
         <h2 className="text-2xl font-bold text-left">About Me</h2>
         <p className="text-left mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
         <h3 className="text-xl font-semibold text-left mt-6">Contact Details</h3>
@@ -61,7 +48,7 @@ const About = () => {
       </div>
 
       {/* Image Section */}
-      <div className="lg:w-1/3 px-4 mb-6 lg:mb-0 flex justify-center">
+      <div className="lg:w-1/4 px-4 mb-6 lg:mb-0 flex justify-center">
         <Image 
           src="/avatar2.png" 
           alt="Profile Picture" 
@@ -72,7 +59,7 @@ const About = () => {
       </div>
 
       {/* Skills Section */}
-      <div className="skills-section lg:w-1/3 px-4">
+      <div className="skills-section lg:w-1/4 px-4">
         <h3 className="text-2xl font-bold text-left">My Skills</h3>
         {skills.map((skill, index) => (
           <SkillBar key={index} skill={skill.name} level={skill.level} />
