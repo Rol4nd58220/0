861188b3279dedb93b5569ccd076db8443a30f0b
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 07:40:57 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -14,7 +14,7 @@ import Circles from '../../components/Circles';
 // SkillBar component for displaying skills
 const SkillBar = ({ skill, level }) => (
   <div className="mb-6">
-    <div className="text-lg font-medium text-center">{skill}</div>
+    <div className="text-lg font-medium text-left">{skill}</div>
     <div className="mt-2 w-full bg-gray-200 rounded-full h-4">
       <div className="bg-blue-600 h-4 rounded-full" style={{ width: `${level}%` }}></div>
     </div>
@@ -31,37 +31,39 @@ const About = () => {
   ];
 
   return (
-    <div className="container mx-auto flex flex-col lg:flex-row items-center justify-around py-6">
-      {/* Bio Section */}
-      <div className="lg:w-1/4 px-4 my-6 text-center">
-        <h2 className="text-2xl font-bold">About Me</h2>
-        <p className="mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
-        <h3 className="text-xl font-semibold mt-6">Contact Details</h3>
-        <ul className="list-none mt-10">
-          <li className="flex items-center justify-center mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
-          <li className="flex items-center justify-center mb-2"><FaPhone className="mr-2" /> +123456789</li>
-          <li className="flex items-center justify-center mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
-          <li className="flex items-center justify-center mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
-        </ul>
-      </div>
+    <div className="container mx-auto py-6 h-full">
+      <div className="flex flex-col lg:flex-row justify-around items-center h-full">
+        {/* Bio Section */}
+        <div className="lg:w-1/4 px-4 text-center mx-auto">
+          <h2 className="text-2xl font-bold">About Me</h2>
+          <p className="mt-4">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
+          <h3 className="text-xl font-semibold mt-6">Contact Details</h3>
+          <ul className="list-none mt-4">
+            <li className="flex items-center justify-center mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
+            <li className="flex items-center justify-center mb-2"><FaPhone className="mr-2" /> +123456789</li>
+            <li className="flex items-center justify-center mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
+            <li className="flex items-center justify-center mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
+          </ul>
+        </div>
 
-      {/* Image Section */}
-      <div className="lg:w-1/4 px-4 my-6 flex justify-center">
-        <Image 
-          src="/avatar2.png" 
-          alt="Profile Picture" 
-          width={200}
-          height={200}
-          className="rounded-full"
-        />
-      </div>
+        {/* Image Section */}
+        <div className="lg:w-1/4 px-4 flex justify-center mx-auto">
+          <Image 
+            src="/avatar2.png" 
+            alt="Profile Picture" 
+            width={200}
+            height={200}
+            className="rounded-full"
+          />
+        </div>
 
-      {/* Skills Section */}
-      <div className="skills-section lg:w-1/4 px-4 my-6 text-center">
-        <h3 className="text-2xl font-bold">My Skills</h3>
-        {skills.map((skill, index) => (
-          <SkillBar key={index} skill={skill.name} level={skill.level} />
-        ))}
+        {/* Skills Section */}
+        <div className="skills-section lg:w-1/4 px-4 text-center mx-auto">
+          <h3 className="text-2xl font-bold">My Skills</h3>
+          {skills.map((skill, index) => (
+            <SkillBar key={index} skill={skill.name} level={skill.level} />
+          ))}
+        </div>
       </div>
     </div>
   );
