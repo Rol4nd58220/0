29dc4f733190f22dffcbd2162b7691d73f8feddb
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 06:50:29 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -8,6 +8,10 @@ import {
   FaReact,
   FaWordpress,
   FaFigma,
+  FaEnvelope, // For email icon
+  FaPhone, // For phone icon
+  FaLinkedin, // For LinkedIn icon
+  FaGithub, // For GitHub icon
 } from "react-icons/fa";
 
 import {
@@ -25,9 +29,20 @@ import Circles from '../../components/Circles';
 const About = () => {
   const [index, setIndex] = useState(0);
   return (
-    <div className='bg-primary/60 h-full '>
+    <div className="bg-primary/60 h-full container mx-auto flex flex-col lg:flex-row">
       <Circles />
-      {/* Avatar img */}
+      {/* Avatar and Bio Section */}
+      <div className="bio-section lg:w-1/2 px-4 py-6">
+        <h2 className="text-2xl font-bold text-left">About Me</h2>
+        <p className="text-left mt-4"> I'm a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
+        <h3 className="text-xl font-semibold text-left mt-6">Contact Details</h3>
+        <ul className="list-none mt-4">
+          <li className="flex items-center mb-2"><FaEnvelope className="mr-2" /> email@example.com</li>
+          <li className="flex items-center mb-2"><FaPhone className="mr-2" /> +123456789</li>
+          <li className="flex items-center mb-2"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
+          <li className="flex items-center mb-2"><FaGithub className="mr-2" /> GitHubProfile</li>
+        </ul>
+      </div>
     </div>
   );
 };
