c1bb917865dbc63ceffbd786b576417f106ea334
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 12:59:00 +0800

    slider04

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,57 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Services = () => {
-  const [items, setItems] = useState([]); // State to store your items
-  const [page, setPage] = useState(0);    // State to track the current page
-  const loader = useRef(null);
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     // Function to fetch data
-    const fetchMoreData = () => {
+    const fetchMoreData = (newPage) => {
       // Fetch data and update state
       // This is a mock function, replace with your data fetching logic
-      const moreItems = new Array(10).fill(null).map((_, index) => `Item ${(page * 10) + index + 1}`);
-      setItems(prevItems => [...prevItems, ...moreItems]);
-      setPage(prevPage => prevPage + 1);
+      const moreItems = new Array(10).fill(null).map((_, index) => `Item ${newPage * 10 + index + 1}`);
+      setItems(moreItems);
     };
 
-    // IntersectionObserver callback
-    const handleObserver = (entities) => {
-      const target = entities[0];
-      if (target.isIntersecting) {
-        fetchMoreData();
-      }
-    };
+    fetchMoreData(page);
+  }, [page]); // Dependency array ensures effect runs on page change
 
-    // Creating an IntersectionObserver
-    const option = {
-      root: null,
-      rootMargin: "20px",
-      threshold: 1.0
-    };
-    const observer = new IntersectionObserver(handleObserver, option);
+  // Function to handle page changes
+  const handlePrevPage = () => {
+    setPage((prevPage) => Math.max(prevPage - 1, 0));
+  };
 
-    // Observing the 'loader' element
-    if (loader && loader.current) {
-      observer.observe(loader.current);
-    }
-
-    // Cleanup observer on unmount
-    return () => {
-      observer.disconnect();
-    };
-  }, []); // Empty array ensures effect runs only once on mount
+  const handleNextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h2>Services</h2>
-      <div>
-        {items.map((item, index) => (
-          <div key={index}>{item}</div>
-        ))}
+      <div className="flex items-center justify-center my-4">
+        <button onClick={handlePrevPage} className="mr-4 p-2 bg-gray-200 rounded">Prev</button>
+        <div className="flex flex-col items-center">
+          {items.map((item, index) => (
+            <div key={index} className="p-2 m-2 border rounded shadow">{item}</div>
+          ))}
+        </div>
+        <button onClick={handleNextPage} className="ml-4 p-2 bg-gray-200 rounded">Next</button>
       </div>
-      {/* Loader element at the bottom */}
-      <div ref={loader} />
     </div>
   );
 };
