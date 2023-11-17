f1bb6cc5ec16d58db25881dd2344fa6bb83ab70b
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 13:10:42 +0800

    slider06

--- pages/services/index.js
+++ pages/services/index.js
@@ -12,19 +12,21 @@ const Services = () => {
     setActiveIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
   };
 
+
   return (
     <div className="flex items-center justify-center h-screen">
       <button onClick={handlePrev} className="p-2 bg-gray-200 rounded">Prev</button>
       
-      <div className="relative mx-4">
+      <div className="flex relative mx-4">
         {items.map((item, index) => (
           <div 
             key={index}
-            className={`absolute top-0 left-1/2 transform -translate-x-1/2 w-64 h-32 p-4 border rounded transition-all duration-500 ${index === activeIndex ? 'opacity-100 scale-100' : 'opacity-50 scale-75'}`}
+            className={`absolute w-64 h-32 p-4 border rounded transition-all duration-500 ${index === activeIndex ? 'opacity-100 scale-100' : 'opacity-50 scale-75'}`}
             style={{
               zIndex: index === activeIndex ? 2 : 1,
               filter: index === activeIndex ? 'none' : 'blur(4px)',
-              transitionDelay: index === activeIndex ? '0.1s' : '0s'
+              left: `${index === activeIndex ? 50 : (index < activeIndex ? 40 : 60)}%`, // Adjust the position based on the index
+              transform: `translateX(-50%)`
             }}
           >
             {item}
