efa656d122e5a27ed0e02b9ce3e75e3c8a0116e0
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 08:07:24 +0800

    Avatar2

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,6 +10,8 @@ import {
 } from "react-icons/fa";
 
 import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants'
 
 // SkillBar component for displaying skills
 const SkillBar = ({ skill, level }) => (
@@ -31,10 +33,11 @@ const About = () => {
   ];
 
   return (
-    <div className="bg-primary/60 container mx-auto py-6 h-full">
+    <div className="container mx-auto py-6 h-full">
       <div className="flex flex-col lg:flex-row justify-around items-center h-full">
         {/* Bio Section */}
-        <div className="lg:w-1/4 px-4 mx-auto">
+        <motion.div variants={fadeIn('down', 0.2)} initial="hidden" animate="show"
+        exit="hidden" className="lg:w-1/4 px-4 mx-auto">
           <h2 className="text-3xl font-bold">About Me</h2>
           <p className="mt-4 text-xl">Im a passionate web developer with a focus on modern JavaScript technologies. I love creating interactive and dynamic user experiences.</p>
           <h3 className="text-3xl font-semibold mt-6">Contact Details</h3>
@@ -44,10 +47,11 @@ const About = () => {
             <li className="flex items-center justify-start mb-2 text-xl"><FaLinkedin className="mr-2" /> LinkedInProfile</li>
             <li className="flex items-center justify-start mb-2 text-xl"><FaGithub className="mr-2" /> GitHubProfile</li>
           </ul>
-        </div>
+        </motion.div>
 
         {/* Image Section */}
-        <div className="bg-primary/60 lg:w-1/4 px-4 flex justify-center mx-auto">
+        <motion.div variants={fadeIn('up', 0.5)} initial="hidden" animate="show"
+        exit="hidden" transition={{duration: 1, ease: 'easeInOut'}} className="lg:w-1/4 px-4 flex justify-center mx-auto">
           <Image 
             src="/avatar2.png" 
             alt="Profile Picture" 
@@ -55,15 +59,16 @@ const About = () => {
             height={1200}
             className="rounded-full"
           />
-        </div>
+        </motion.div>
 
         {/* Skills Section */}
-        <div className="bg-primary/60 skills-section lg:w-1/4 px-4 text-center mx-auto">
+        <motion.div variants={fadeIn('down', 0.2)} initial="hidden" animate="show"
+        exit="hidden"className="skills-section lg:w-1/4 px-4 text-center mx-auto">
           <h3 className="text-3xl font-bold mb-2">My Skills</h3>
           {skills.map((skill, index) => (
             <SkillBar key={index} skill={skill.name} level={skill.level} />
           ))}
-        </div>
+        </motion.div>
       </div>
     </div>
   );
