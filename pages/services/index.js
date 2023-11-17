921830128245127f84c63f5ff74d49be86287fe9
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 14:55:22 +0800

    slider14

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,50 +1,46 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { FreeMode, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/free-mode';
 
-import { FreeMode, Pagination } from 'swiper/modules';
+// Import or define ServiceData
+import ServiceData from './constant'; // Adjust the path as necessary
 
-import { RxArrowTopRight } from 'react-icons/rx';
-import { ServiceData } from '../constants';
+// ... (RxArrowTopRight usage if needed)
 
 const Services = () => {
   return (
     <div className='flex items-center justify-center flex-col h-screen bg-[#6c34af]'>
       <Swiper
-          breakpoints={{
-            340: {
-              slidePerview: 2,
-              spaceBetween: 15
-            },
-            700: {
-              slidePerview: 3,
-              spaceBetween: 15
-            }
-          }}
-
-          freeMode={true}
-          pagination={{
-            clickable: true
-          }}
-          modules={{FreeMode, Pagination}}
-          className='max-w-[90%] lg:max-w-[80%]'
+        breakpoints={{
+          340: {
+            slidesPerView: 2,
+            spaceBetween: 15
+          },
+          700: {
+            slidesPerView: 3,
+            spaceBetween: 15
+          }
+        }}
+        freeMode={true}
+        pagination={{
+          clickable: true
+        }}
+        modules={[FreeMode, Pagination]}
+        className='max-w-[90%] lg:max-w-[80%]'
       >
-          {ServiceData.map((item) => (
-            <SwiperSlide key={item.title}>
-              <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px]'>
-
-              </div>
-
-            </SwiperSlide>
-          ))}
-
+        {ServiceData.map((item) => (
+          <SwiperSlide key={item.title}>
+            <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px]'>
+              {/* Slide content here, using item */}
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
-
     </div>
-  )
-}
+  );
+};
 
 export default Services;
