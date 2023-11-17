5446ae77290aeec0674ea48e46895bb2e0794e2c
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 12:38:51 +0800

    slider01

--- pages/services/index.js
+++ pages/services/index.js
@@ -6,58 +6,54 @@ const Services = () => {
   const galleryRef = useRef();
   const nextButtonRef = useRef();
   const prevButtonRef = useRef();
+  const cardsRef = useRef([]);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    const spacing = 0.1;
-    const snap = gsap.utils.snap(spacing);
-    const cards = gsap.utils.toArray('.cards li');
-    const seamlessLoop = buildSeamlessLoop(cards, spacing);
-    const scrub = gsap.to(seamlessLoop, {
-      totalTime: 0,
-      duration: 0.5,
-      ease: "power3",
-      paused: true
-    });
+    const cards = cardsRef.current;
 
-    const trigger = ScrollTrigger.create({
-      start: 0,
-      onUpdate(self) {
-        // ... (existing logic)
-      },
-      end: "+=3000",
-      pin: galleryRef.current
-    });
-
-    // ... (existing functions: wrapForward, wrapBackward, scrubTo, buildSeamlessLoop)
-
-    nextButtonRef.current.addEventListener("click", () => scrubTo(scrub.vars.totalTime + spacing));
-    prevButtonRef.current.addEventListener("click", () => scrubTo(scrub.vars.totalTime - spacing));
-
-    // Cleanup function
-    return () => {
-      if (nextButtonRef.current && prevButtonRef.current) {
-        nextButtonRef.current.removeEventListener("click", scrubTo);
-        prevButtonRef.current.removeEventListener("click", scrubTo);
+    // Create a simple horizontal scroll effect
+    gsap.to(cards, {
+      xPercent: -100 * (cards.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: galleryRef.current,
+        pin: true,
+        scrub: 1,
+        snap: 1 / (cards.length - 1),
+        end: () => "+=" + galleryRef.current.offsetWidth
       }
-      trigger.kill();
-    };
+    });
 
+    // Button event listeners for additional control
+    nextButtonRef.current.addEventListener('click', () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.scroll(trigger.scroll() + window.innerWidth));
+    });
+
+    prevButtonRef.current.addEventListener('click', () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.scroll(trigger.scroll() - window.innerWidth));
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
   }, []);
 
   const cardTitles = ["Elementary", "Junior Highschool", "Senior Highschool", "Mother", "Father", "Jesse Martinez"];
 
   return (
-    <div ref={galleryRef} className="gallery">
-      <ul className="cards">
+    <div ref={galleryRef} className="gallery w-full h-screen overflow-hidden">
+      <ul className="cards flex">
         {cardTitles.map((title, index) => (
-          <li key={index}>{title}</li>
+          <li key={index} ref={el => cardsRef.current[index] = el} className="card bg-purple-500 text-white p-4 rounded-lg shadow-lg m-2">
+            {title}
+          </li>
         ))}
       </ul>
-      <div className="actions">
-        <button ref={nextButtonRef} className="next">Next</button>
-        <button ref={prevButtonRef} className="prev">Prev</button>
+      <div className="actions absolute bottom-4 left-1/2 transform -translate-x-1/2">
+        <button ref={nextButtonRef} className="next bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg mr-2">Next</button>
+        <button ref={prevButtonRef} className="prev bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg">Prev</button>
       </div>
     </div>
   );
