e5ba592c16d6bb1c6fadc1b919565c1e54128507
Rol4nd58220 <147376163+Rol4nd58220@users.noreply.github.com>
2023-11-17 15:35:55 +0800

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,14 @@
+import ImageSlider from '../components/ImageSlider';
+
 const Services = () => {
-  return <div>Services</div>;
+  const images = ['/path/to/image1.jpg', '/path/to/image2.jpg', '/path/to/image3.jpg'];
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Services</h1>
+      <ImageSlider images={images} />
+    </div>
+  );
 };
 
 export default Services;
