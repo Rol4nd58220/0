f7bb6e6b427a1007889f68bb4694b0477d7ee133
Rol4nd58220 <147376163+Rol4nd58220@users.noreply.github.com>
2023-11-17 16:05:22 +0800

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -28,7 +28,7 @@ const Services = () => {
         <img
           src={images[currentImage].path}
           alt={images[currentImage].title}
-          className="w-full h-64 object-cover"
+          className="w-full h-64 object-contain"
         />
 
         <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center text-white">
