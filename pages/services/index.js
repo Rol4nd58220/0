664b89be20dd8cc970da0adc751b821254e4ef81
Rol4nd58220 <147376163+Rol4nd58220@users.noreply.github.com>
2023-11-17 16:33:57 +0800

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -24,7 +24,7 @@ const Services = () => {
     <div className="flex flex-col items-center justify-center m-auto w-full">
       <h1 className="flex items-center justify-center text-3xl font-bold mb-4">Achievements</h1>
 
-      <div className="relative w-[200px] h-full object-contain mx-auto">
+      <div className="relative w-full h-full object-contain mx-auto mt-10">
         <img
           src={images[currentImage].path}
           alt={images[currentImage].title}
