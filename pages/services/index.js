501c460082bcdbc0d4ad87013a50994a25d30a6c
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 12:43:19 +0800

    slider03

--- pages/services/index.js
+++ pages/services/index.js
@@ -2,6 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
+
+const cardData = [
+  { title: 'Elementary', image: './elementary.png' },
+  { title: 'Junior Highschool', image: './junior-highschool.png' },
+  // ... Add more cards with titles and images
+  { title: 'Jesse Martinez', image: './jesse-martinez.png' }
+];
+
 const Services = () => {
   const galleryRef = useRef();
   const nextButtonRef = useRef();
@@ -44,19 +52,19 @@ const Services = () => {
 
   return (
     <div ref={galleryRef} className="gallery w-full h-screen overflow-hidden">
-      <ul className="cards flex">
-        {cardTitles.map((title, index) => (
-          <li key={index} ref={el => cardsRef.current[index] = el} className="card bg-purple-500 text-white p-4 rounded-lg shadow-lg m-2">
-            {title}
-          </li>
-        ))}
-      </ul>
-      <div className="actions absolute bottom-4 left-1/2 transform -translate-x-1/2">
-        <button ref={nextButtonRef} className="next bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg mr-2">Next</button>
-        <button ref={prevButtonRef} className="prev bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg">Prev</button>
-      </div>
+    <ul className="cards flex">
+      {cardData.map((card, index) => (
+        <li key={index} ref={el => cardsRef.current[index] = el} className="card bg-purple-500 text-white p-4 rounded-lg shadow-lg m-2 flex flex-col items-center">
+          <img src={card.image} alt={card.title} className="w-24 h-24 object-cover rounded-full mb-4" />
+          <span>{card.title}</span>
+        </li>
+      ))}
+    </ul>
+    <div className="actions absolute bottom-4 left-1/2 transform -translate-x-1/2">
+      <button ref={nextButtonRef} className="next bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg mr-2">Next</button>
+      <button ref={prevButtonRef} className="prev bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg">Prev</button>
     </div>
-  );
-};
+  </div>
+);
 
 export default Services;
