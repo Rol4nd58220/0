4d13aa7cb19e0e2627b385cd1562c7e349aca16b
Rol4nd58220 <147376163+Rol4nd58220@users.noreply.github.com>
2023-11-17 15:39:42 +0800

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,12 +1,55 @@
-import ImageSlider from '../components/ImageSlider';
+import { useState } from 'react';
 
 const Services = () => {
-  const images = ['/path/to/image1.jpg', '/path/to/image2.jpg', '/path/to/image3.jpg'];
+  const images = [
+    { path: '/path/to/image1.jpg', title: 'Title 1' },
+    { path: '/path/to/image2.jpg', title: 'Title 2' },
+    { path: '/path/to/image3.jpg', title: 'Title 3' },
+    { path: '/path/to/image4.jpg', title: 'Title 4' },
+    { path: '/path/to/image5.jpg', title: 'Title 5' },
+    { path: '/path/to/image6.jpg', title: 'Title 6' },
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
 
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Services</h1>
-      <ImageSlider images={images} />
+
+      <div className="relative">
+        <img src={images[currentImage].path} alt={images[currentImage].title} className="w-full h-64 object-cover" />
+
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center text-white">
+          <h2 className="text-xl font-bold mb-2">{images[currentImage].title}</h2>
+          <div className="flex items-center justify-between w-full">
+            <button onClick={prevImage} className="text-white text-2xl focus:outline-none ml-4">
+              &#8249;
+            </button>
+            <button onClick={nextImage} className="text-white text-2xl focus:outline-none mr-4">
+              &#8250;
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        {images.map((image, index) => (
+          <div key={index} className="relative">
+            <img src={image.path} alt={image.title} className="w-full h-40 object-cover mb-2" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-white">
+              <h3 className="text-sm font-bold">{image.title}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
