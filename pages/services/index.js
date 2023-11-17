a41d7bd0c3ee3772c55cf8b38f110ea95ad157a8
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 12:55:02 +0800

    slider03

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,70 +1,59 @@
-import React, { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-
-
-const cardData = [
-  { title: 'Elementary', image: './elementary.png' },
-  { title: 'Junior Highschool', image: './junior-highschool.png' },
-  // ... Add more cards with titles and images
-  { title: 'Jesse Martinez', image: './jesse-martinez.png' }
-];
+import React, { useState, useEffect, useRef } from 'react';
 
 const Services = () => {
-  const galleryRef = useRef();
-  const nextButtonRef = useRef();
-  const prevButtonRef = useRef();
-  const cardsRef = useRef([]);
+  const [items, setItems] = useState([]); // State to store your items
+  const [page, setPage] = useState(0);    // State to track the current page
+  const loader = useRef(null);
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    const cards = cardsRef.current;
-
-    // Create a simple horizontal scroll effect
-    gsap.to(cards, {
-      xPercent: -100 * (cards.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: galleryRef.current,
-        pin: true,
-        scrub: 1,
-        snap: 1 / (cards.length - 1),
-        end: () => "+=" + galleryRef.current.offsetWidth
-      }
-    });
-
-    // Button event listeners for additional control
-    nextButtonRef.current.addEventListener('click', () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.scroll(trigger.scroll() + window.innerWidth));
-    });
-
-    prevButtonRef.current.addEventListener('click', () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.scroll(trigger.scroll() - window.innerWidth));
-    });
-
-    return () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    // Function to fetch data
+    const fetchMoreData = () => {
+      // Fetch data and update state
+      // This is a mock function, replace with your data fetching logic
+      const moreItems = new Array(10).fill(null).map((_, index) => `Item ${(page * 10) + index + 1}`);
+      setItems(prevItems => [...prevItems, ...moreItems]);
+      setPage(prevPage => prevPage + 1);
     };
-  }, []);
 
-  const cardTitles = ["Elementary", "Junior Highschool", "Senior Highschool", "Mother", "Father", "Jesse Martinez"];
+    // IntersectionObserver callback
+    const handleObserver = (entities) => {
+      const target = entities[0];
+      if (target.isIntersecting) {
+        fetchMoreData();
+      }
+    };
+
+    // Creating an IntersectionObserver
+    const option = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+
+    // Observing the 'loader' element
+    if (loader && loader.current) {
+      observer.observe(loader.current);
+    }
+
+    // Cleanup observer on unmount
+    return () => {
+      observer.disconnect();
+    };
+  }, []); // Empty array ensures effect runs only once on mount
 
   return (
-    <div ref={galleryRef} className="gallery w-full h-screen overflow-hidden">
-    <ul className="cards flex">
-      {cardData.map((card, index) => (
-        <li key={index} ref={el => cardsRef.current[index] = el} className="card bg-purple-500 text-white p-4 rounded-lg shadow-lg m-2 flex flex-col items-center">
-          <img src={card.image} alt={card.title} className="w-24 h-24 object-cover rounded-full mb-4" />
-          <span>{card.title}</span>
-        </li>
-      ))}
-    </ul>
-    <div className="actions absolute bottom-4 left-1/2 transform -translate-x-1/2">
-      <button ref={nextButtonRef} className="next bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg mr-2">Next</button>
-      <button ref={prevButtonRef} className="prev bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-lg">Prev</button>
+    <div>
+      <h2>Services</h2>
+      <div>
+        {items.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+      {/* Loader element at the bottom */}
+      <div ref={loader} />
     </div>
-  </div>
-);
+  );
+};
 
-export default services;
+export default Services;
