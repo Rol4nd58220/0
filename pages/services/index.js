ec7bf2073eecd2b3344107091af0536f992aba27
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-17 13:42:44 +0800

    slider08

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,51 +1,80 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Services = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const items = ['Card 1', 'Card 2', 'Card 3'];
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-  const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex + 1) % items.length);
-  };
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 
-  const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
-  };
+import slide_image_1 from './assets/images/img_1.jpg';
+import slide_image_2 from './assets/images/img_2.jpg';
+import slide_image_3 from './assets/images/img_3.jpg';
+import slide_image_4 from './assets/images/img_4.jpg';
+import slide_image_5 from './assets/images/img_5.jpg';
+import slide_image_6 from './assets/images/img_6.jpg';
+import slide_image_7 from './assets/images/img_7.jpg';
 
+function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button onClick={handlePrev} className="p-2 bg-gray-200 rounded">Prev</button>
-      
-      <div className="relative w-full max-w-3xl mx-4">
-        <div className="flex justify-center items-center">
-          {items.map((item, index) => {
-            let scale = 1;
-            let opacity = 1;
-            if (index !== activeIndex) {
-              scale = 0.8;
-              opacity = 0.5;
-            }
-            return (
-              <div
-                key={index}
-                style={{
-                  transform: `translateX(${(index - activeIndex) * 150}px) scale(${scale})`,
-                  opacity: opacity,
-                  transition: 'transform 0.5s, opacity 0.5s',
-                  position: 'absolute',
-                }}
-                className="w-64 h-32 p-4 border rounded"
-              >
-                {item}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div className="container">
+      <h1 className="heading">Flower Gallery</h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
 
-      <button onClick={handleNext} className="p-2 bg-gray-200 rounded">Next</button>
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
     </div>
   );
-};
+}
 
-export default Services;
+export default App;
