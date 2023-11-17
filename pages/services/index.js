3db8578ddefa31fefa9ab4ab3ea055b1cd39ca72
Rol4nd58220 <147376163+Rol4nd58220@users.noreply.github.com>
2023-11-17 15:58:36 +0800

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -28,7 +28,7 @@ const Services = () => {
         <img
           src={images[currentImage].path}
           alt={images[currentImage].title}
-          className={`w-full h-64 object-cover ${currentImage === 0 ? 'block' : 'hidden'}`}
+          className="w-full h-64 object-cover"
         />
 
         <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center text-white">
@@ -42,17 +42,6 @@ const Services = () => {
             </button>
           </div>
         </div>
-      </div>
-
-      <div className="grid grid-cols-2 gap-4 mt-4">
-        {images.map((image, index) => (
-          <div key={index} className={`relative ${index === currentImage ? 'hidden' : 'block'}`}>
-            <img src={image.path} alt={image.title} className="w-full h-40 object-cover mb-2" />
-            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-white">
-              <h3 className="text-sm font-bold">{image.title}</h3>
-            </div>
-          </div>
-        ))}
       </div>
     </div>
   );
