e7f57324477fd20caebdf681cc0ef5f4154d6f5e
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-21 19:45:23 +0800

    portfolio

--- pages/services/index.js
+++ pages/services/index.js
@@ -10,9 +10,9 @@ import { fadeIn } from '../../variants';
 
 const Services = () => {
   return <div className='h-full bg-primary/30 py-36 flex items-center '>
-    <Circles />
-      <div className="container mx-auto"></div>
-      <Bulb />
+    {/* <Circles /> */}
+    <div className="container mx-auto"></div>
+    <Bulb />
   </div>;
 };
 
