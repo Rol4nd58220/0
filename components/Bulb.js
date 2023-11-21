92187440feb00f01c7dc5ab0a8591e440939b07c
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-21 19:56:11 +0800

    portfolio

--- components/Bulb.js
+++ components/Bulb.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Bulb = () => {
   return <div className='absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dogde animate-pulse duration-75 z-10 w-{200px} xl:w-{260px}'>
-    <Image src={'./bulb.png'} width={260} heigh={200} className='w-full h-full' alt=''/>
+    <Image src={'./public/bulb.png'} width={260} heigh={200} className='w-full h-full' alt=''/>
   </div>;
 };
 
