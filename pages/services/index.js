3f84760db787507bc2ba969334c79ac3706dd45d
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-21 19:04:33 +0800

    services

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,50 +1,20 @@
-import { useState } from 'react';
+
+
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circle from '..//../components/Circles';
+
+// Framer Motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 
 const Services = () => {
-  const images = [
-    { path: './elementary.jpg', title: 'Elementary' },
-    { path: './juniorHighSchool.jpg', title: 'Junior Highschool' },
-    { path: './SeniorHighschool.jpg', title: 'Senior Highschool' },
-    { path: './belovedMother.png', title: 'Beloved Mother' },
-    { path: './greatDad.png', title: 'Beloved Father' },
-    { path: './jesseMartinez.jpg', title: 'Jessie Martirez' },
-  ];
-
-  const [currentImage, setCurrentImage] = useState(0);
-
-  const nextImage = () => {
-    setCurrentImage((prev) => (prev + 1) % images.length);
-  };
-
-  const prevImage = () => {
-    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center mx-auto w-full max-w-screen-lg">
-      <h1 className="text-3xl font-bold my-4">Achievements</h1>
-
-      <div className="relative w-full h-full overflow-hidden rounded-lg shadow-md">
-        <img
-          src={images[currentImage].path}
-          alt={images[currentImage].title}
-          className="w-full h-full object-cover"
-        />
-
-        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center text-white">
-          <h2 className="text-xl font-bold mb-2">{images[currentImage].title}</h2>
-          <div className="flex items-center justify-between w-full">
-            <button onClick={prevImage} className="text-white text-2xl focus:outline-none ml-4">
-              &#8249;
-            </button>
-            <button onClick={nextImage} className="text-white text-2xl focus:outline-none mr-4">
-              &#8250;
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className='h-full bg-primary/30 py-36 flex items-center '>
+    <Cicles>
+      <div className="container mx-auto"></div>
+      <Bulb />
+    </Cicles>
+  </div>;
 };
 
 export default Services;
