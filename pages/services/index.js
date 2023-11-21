0e65b9badf1fc263dbb439b3e03683b0430ce125
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-21 20:05:03 +0800

    portfolio

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,8 +1,8 @@
 
 
 import ServiceSlider from '../../components/ServiceSlider';
-import Bulb from '../../components/Bulb';
-import Circles from '../../components/Circles';
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
 
 // Framer Motion
 import { motion } from 'framer-motion';
@@ -10,7 +10,7 @@ import { fadeIn } from '../../variants';
 
 const Services = () => {
   return (
-    <div className='h-full bg-primary/60 py-36 flex items-center '>
+    <div className="bg-primary/60 h-full py-36 flex items-center ">
       <Circles />
       <div className="container mx-auto"></div>
       <Bulb />
